bce03d28710647860e12bb33841ed0e329c69337
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-04-19 12:59:37 +0200

    update

--- TypeScriptDemo/datatypes.ts
+++ TypeScriptDemo/datatypes.ts
@@ -15,6 +15,24 @@ let dob = "25";
 
 let result = parseInt(dob);
 
-let isValid: boolean;
+let isValid: boolean = false;
+
+let empList: string[];
+
+empList = ["Santosh", "Santosh1", "Santosh2"];
+
+let numList: Array<number>; // ts generics
+
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let results = numList.filter((num) => num > 2);
+
+let num = numList.find((num) => num === 2);
+
+let sum = numList.reduce((acc, num) => acc + num);
+
+console.log(results);
+console.log(num);
+console.log(sum);
 
 // lname = 10;
