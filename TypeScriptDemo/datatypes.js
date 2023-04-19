bcbac2ac570f08e06cc782a3889f618f44f6832b
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-04-19 12:45:16 +0200

    update

---
+++ TypeScriptDemo/datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+let lname;
+lname = "Santosh";
+let newname = lname.toUpperCase();
+console.log(newname);
+// lname = 10;
--- TypeScriptDemo/datatypes.ts
+++ TypeScriptDemo/datatypes.ts
@@ -1,3 +1,20 @@
-let lname = "John";
+let lname: string;
+
+lname = "Santosh";
+
+let newname = lname.toUpperCase();
+
+console.log(newname);
+
+let age: number;
+
+age = 25;
+age = 25.5;
+
+let dob = "25";
+
+let result = parseInt(dob);
+
+let isValid: boolean;
 
 // lname = 10;
